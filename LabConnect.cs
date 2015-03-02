d1598de0d051ebb22f817c755732a21be4a77d40
Hendrik Lüth <Kontakt@lueth-labs.de>
2015-03-02 14:26:00 +0100

    add the css file for the dll

---
+++ LabConnect.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LabConnect
+{
+    public class USB
+    {
+    }
+
+    public class Sgen
+    {
+    }
+
+    public class Psup
+    {
+    }
+}
