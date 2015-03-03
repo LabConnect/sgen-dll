a362889b4e843d2bde93c08346bdb0c04bb4eef8
Hendrik Lüth <Kontakt@lueth-labs.de>
2015-03-03 14:39:53 +0100

    add the missing functions, should be complete now

--- LabConnect/LabConnect/Class1.cs
+++ LabConnect/LabConnect/Class1.cs
@@ -12,7 +12,9 @@ namespace LabConnect
 
     public class Sgen
     {
+        //definition der klassenvariablen
         byte[] output_data = {0x20, 0x00, 0x66, 0x49, 0x01, 0x40, 0xD4, 0xD5, 0x80, 0x7F, 0x01};
+        bool rechteck = false;
 
         //registerwerte für die Digipotis für die Ausgangsspannung berechnen
         void RegwertUout(int u_amplitude)
@@ -67,17 +69,63 @@ namespace LabConnect
         //zwischen Rechteck und Sinus/Dreieck unterscheiden
         void SetWaveform(int form)
         {
-
+            if (form == 2)
+            {
+                rechteck = true;
+            }
+            else
+            {
+                rechteck = false;
+            }
+            return;
         }
 
         //Frequenzregister des AD9833 berechnen
         void SetFrequency(int frequenz)
         {
+            float mclk = 25000000, register_size = 268435456;
+            float teiler = mclk / register_size;
+            int f_regwert = Convert.ToInt32(frequenz / teiler);
+            
+            //block1=lsb --> block4 = msb
+            byte block1, block2, block3, block4;
 
+            block1 = Convert.ToByte(f_regwert);
+            f_regwert = f_regwert >> 8;
+            block2 = Convert.ToByte(f_regwert);
+            block2 = Convert.ToByte((block2 | 0x40) & (~0x80));
+            f_regwert = f_regwert >> 6;
+            block3 = Convert.ToByte(f_regwert);
+            f_regwert = f_regwert >> 8;
+            block4 = Convert.ToByte(f_regwert);
+            block4 = Convert.ToByte((block4 | 0x40) & (~0x80));
+            
+            if (rechteck == true)
+            {
+                output_data[10] = 3;
+            }
+            else
+            {
+                if (frequenz <= 500)
+                {
+                    output_data[10] = 3;
+                }
+                else if(frequenz <= 10000)
+                {
+                    output_data[10] = 2;
+                }
+                else
+                {
+                    output_data[10] = 1;
+                }
+            }
         }
-    }
-
-    public class Psup
-    {
+        
+        //Daten senden
+        void CommitData()
+        {
+            //this is where we call the usbfunction
+            return;
+        }
     }
 }
